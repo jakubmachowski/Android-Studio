01c0161b07fba8a9af4c04366bbdfff6234495e1
Jakub Machowski <machowski.jakub@o2.pl>
2018-06-16 22:00:24 +0200

    W pełni działająca aplikacja

--- Cw1/app/src/main/java/com/example/jmach/cw1/MainActivity.java
+++ Cw1/app/src/main/java/com/example/jmach/cw1/MainActivity.java
@@ -4,9 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -50,11 +51,41 @@ public class MainActivity extends AppCompatActivity {
     //Uruchamiany gdy działanie zakończy działanie.
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        Log.i("Finish", "test");
         super.onActivityResult(requestCode, resultCode, intent);
         if (resultCode == Activity.RESULT_OK) {
-            String message = intent.getStringExtra("GradesNumber");
-            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            Double average = Double.parseDouble(intent.getStringExtra("GradesNumber"));
+            showAverage(average);
         }
+    }
+
+    private void showAverage(Double average) {
+        TextView averageTextView = findViewById(R.id.averageTextView);
+        averageTextView.setVisibility(View.VISIBLE);
+        averageTextView.setText(String.format("Twoja średnia to: %.2f", average));
+
+        EditText editTextName = findViewById(R.id.inputName);
+        editTextName.setEnabled(false);
+        EditText editTextSurname = findViewById(R.id.inputSurname);
+        editTextSurname.setEnabled(false);
+        EditText editTextGrades = findViewById(R.id.inputGrade);
+        editTextGrades.setEnabled(false);
+
+        final String messange;
+        Button button = findViewById(R.id.buttonGrade);
+        if (average <= 3) {
+            button.setText("Tym razem mi nie poszło.");
+            messange = "Wysyłam podanie o zaliczenie warunkowe.";
+        } else {
+            button.setText("Super! :)");
+            messange = "Gratulacje! Otrzymujesz zaliczene!";
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, messange, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
     }
 }
